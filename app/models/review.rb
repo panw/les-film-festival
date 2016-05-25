249b36c7dfb129f19e46df8db9403fa30a60363a
Pan Wangperawong <panthongw@gmail.com>
2016-05-25 16:11:20 -0400

    added virtual stars attributes to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,12 @@ class Review < ActiveRecord::Base
   belongs_to :film
   belongs_to :user
   has_many :comments
+
+  def stars
+  	self.rating.to_f / 2
+  end
+
+  def stars=(stars)
+  	self.rating = stars * 2
+  end
 end
