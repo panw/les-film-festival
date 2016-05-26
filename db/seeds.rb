9ff347b8ff5abf3c93796635ebe3815290634984
Sam Blackman <sblackman@gmail.com>
2016-05-25 20:55:58 -0400

    add users, reviews and comments seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,30 @@ thriller_horror = Category.create(name: 'Thriller/Horror')
 musical = Category.create(name: 'Musical')
 documentary = Category.create(name: 'Documentary')
 
+bobby = User.create!(first_name: "Bobby", last_name: "Je Niro", role: :admin, email: "bobby@lesfilmfest.org", password: "password")
+peo = User.create!(first_name: "Peo", last_name: "PiCaprio", role: :admin, email: "peo@lesfilmfest.org", password: "password")
+iuliia = User.create!(first_name: "Iuliia", last_name: "Roberts", role: :judge, email: "iuliia@lesfilmfest.org", password: "password")
+ryan = User.create!(first_name: "Ryan", last_name: "Ronalds", role: :judge, email: "ryan@lesfilmfest.org", password: "password")
+zack = User.create!(first_name: "Zack", last_name: "Praff", role: :judge, email: "zack@lesfilmfest.org", password: "password")
+
+
 [action_adventure, drama, comedy, thriller_horror, musical, documentary].each do |category|
-	5.times do
+	users = [bobby, peo, iuliia, ryan, zack].shuffle
+  5.times do
 		film = Film.new(
 			name: Faker::Book.title, 
 			description: Faker::Hipster.paragraph, 
-			category: category
+			category: category,
+      reviews: (1..5).map { Review.create!(rating: rand(0..10), content: Faker::Hipster.paragraph, user: users.pop) }
 		)
 		film.image_url = Faker::Avatar.image(film.name.split(' ').join('-'), '280x420')
 		film.save!
 	end
 end
 
+Review.all.each do |review|
+  users = [bobby, peo, iuliia, ryan, zack].shuffle
+  rand(0..users.size).times do
+    Comment.create!(content: Faker::Hipster.paragraph, user: users.pop, review: review)
+  end
+end
